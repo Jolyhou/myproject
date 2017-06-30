8537b2985fec6b1a98eb38098e8669bde8ec4e6c
houjiaoli <houjiaoli@bilibili.com>
2017-06-30 19:09:52 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
+// 定义名为 todo-item 的新组件
+Vue.component('todo-item', {
+  template: '<li>这是个待办项</li>'
+})
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    title: '实时加载' + new Date(),
+    isButton: false
+  },
+  created () {
+    console.log('now', this.title)
   }
 })
